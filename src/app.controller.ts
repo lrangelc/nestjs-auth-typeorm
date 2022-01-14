51908069e1eb107b99b13845bd38f2a8570e92c2
Luis Rangel <luisrangelc@gmail.com>
2022-01-14 15:07:54 -0600

    typeORM added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -19,4 +19,9 @@ export class AppController {
   hello() {
     return 'con /sas/';
   }
+
+  @Get('tasks')
+  getTasks(): Promise<any> {
+    return this.appService.getTasks();
+  }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,25 @@
 import { Module, Global } from '@nestjs/common';
+import { Client } from 'pg';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
+
+const client = new Client({
+  host: 'localhost',
+  database: 'my_DB',
+  port: 5432,
+  user: 'root',
+  password: '123456',
+});
+
+client.connect();
+// client.query(`SELECT * FROM tasks`, (err, res) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(res.rows);
+// });
 
 @Global()
 @Module({
@@ -10,7 +28,11 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
