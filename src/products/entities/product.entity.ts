7d2d261820f113fd5652524b99e0a3e1a8befdee
Luis Rangel <luisrangelc@gmail.com>
2022-01-24 22:51:08 -0600

    feat: naiming

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -7,12 +7,14 @@ import {
   ManyToOne,
   ManyToMany,
   Index,
+  JoinColumn,
+  JoinTable,
 } from 'typeorm';
 
 import { Brand } from './brand.entity';
 import { Category } from './category.entity';
 
-@Entity({ name: 'product' })
+@Entity({ name: 'products' })
 @Index(['price', 'stock'])
 export class Product {
   @PrimaryGeneratedColumn()
@@ -36,20 +38,36 @@ export class Product {
   image: string;
 
   @CreateDateColumn({
+    name: 'created_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   createdAt: Date;
 
   @UpdateDateColumn({
+    name: 'updated_at',
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   updatedAt: Date;
 
   @ManyToOne(() => Brand, (brand) => brand.products)
+  @JoinColumn({ name: 'brand_id' })
   brand: Brand;
 
-  @ManyToMany(() => Category, (category) => category.products)
+  @ManyToMany(() => Category, (categories) => categories.products, {
+    nullable: true,
+  })
+  @JoinTable({
+    name: 'product_categories',
+    joinColumn: {
+      name: 'product_id',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'category_id',
+      referencedColumnName: 'id',
+    },
+  })
   categories: Category[];
 }
