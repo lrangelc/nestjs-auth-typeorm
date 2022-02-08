19d0b0168f604925f2855ac47705c8cd56a939fb
Luis Rangel <luisrangelc@gmail.com>
2022-02-07 19:14:13 -0600

    feat: add decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,31 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  SetMetadata,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { Public, IS_PUBLIC_KEY } from './auth/decorators/public.decorator';
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @SetMetadata(IS_PUBLIC_KEY, true)
   @Get('nuevo')
   newEndpoint() {
     return 'yo soy nuevo';
   }
 
+  @Public()
   @Get('/ruta/')
   hello() {
     return 'con /sas/';
